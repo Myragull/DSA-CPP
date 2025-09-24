8e62a77132e028eb55d225fb94132bbd06277538
Myra Gull <myragull749@gmail.com>
2025-09-24 07:59:47 +0500

    Added 5 more qs of patterns

--- Day4/day4.cpp
+++ Day4/day4.cpp
@@ -26,7 +26,6 @@ int main()
     //     i++;
     // }
 
-
      //2
     // 3 2 1  
     // 3 2 1   
@@ -47,7 +46,6 @@ int main()
     //     cout << endl;
     //     i++;
     // }
-
 
     //3
     // 1 2 3
@@ -71,7 +69,6 @@ int main()
     // cout << endl;
     // i++;
     // }
-
     
     //4
     //* 
@@ -93,7 +90,6 @@ int main()
     //     cout<<endl;
     //     i++;
     // }
-
 
     //5
     // 1
@@ -118,7 +114,6 @@ int main()
     //     cout<<endl;
     //     i++;
     // }
-
 
     //6
     // 1
@@ -166,7 +161,108 @@ int main()
     //     i++;
     // }
 
+    // 7
+    // 1
+    // 2 1
+    // 3 2 1
+    // 4 3 2 1 
 
+    // int n;
+    // cout<<"Enter number : ";
+    // cin>>n;
+
+    // int i=1;
+    // while(i<=n){
+    //     int count =i;
+    //     int j=1;
+    //     while(j<=i){
+    //         cout<<count<<" ";
+    //         count--;
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+
+    //8
+    // A A A 
+    // B B B
+    // C C C
+    // D D D
+     
+    // method 1
+    // int n ;
+    // cout<<"Enter number : ";
+    // cin>>n;
+    // int i=1;
+    // while(i<=n){
+    //     char ch = 'A'+i -1;
+    //     int j=1;
+    //     while(j<=n){
+    //         cout<<ch;
+    //         j++;
+    //     }
+    //     // ch++;
+    //     cout<<endl;
+    //     i++;
+    // }
+
+    // methhod 2
+    // int n ;
+    // cout<<"Enter number : ";
+    // cin>>n;
+    // char ch='A';
+    // int i=1;
+    // while(i<=n){
+    //     int j=1;
+    //     while(j<=n){
+    //         cout<<ch;
+    //         j++;
+    //     }
+    //     ch++;
+    //     cout<<endl;
+    //     i++;
+    // }
+
+    // 9
+    //  A B C
+    //  A B C
+    //  A B C
+ 
+    // method 1
+    // int n ;
+    // cout<<"Enter number : ";
+    // cin>>n;
+    // int i=1;
+    // while(i<=n){
+    //     // dont set it globally instead set it inside of loop 
+    //     char ch='A';
+    //     int j=1;
+    //     while(j<=n){
+    //         cout<<ch;
+    //         ch++;
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+
+    // method 2
+    // int n ;
+    // cout<<"Enter number : ";
+    // cin>>n;
+    // int i=1;
+    // while(i<=n){
+    //     int j=1;
+    //     while(j<=n){
+    //         char ch = 'A'+j -1;
+    //         cout<<ch;
+    //         j++;
+    //     }
+    //     // ch++;
+    //     cout<<endl;
+    //     i++;
+    // }
 
     return 0;
 }
