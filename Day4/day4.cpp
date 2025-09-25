f538688e75d4cf672ae5c0b1470a2eec2367785c
Myra Gull <myragull749@gmail.com>
2025-09-25 09:46:41 +0500

    Added more qs for patterns uisg while lopps

--- Day4/day4.cpp
+++ Day4/day4.cpp
@@ -209,5 +209,204 @@ int main(){
     //     i++;
     // }
 
+    // 8
+    // 1  
+    // 2  1
+    // 3  2  1
+    // 4  3  2  1
+    // int n;
+    // cout<<"Enter a number : ";
+    // cin>>n;
+
+    // int i=1;
+    // while(i<=n){
+
+    //     int j=1;
+    //     while(j<=i){
+    //         cout<<(i-j+1)<<"  ";
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+
+    
+    // 9
+    // AAAA
+    // BBBB
+    // CCCC
+    // DDDD
+    // int n;
+    // cout<<"Enter a number :";
+    // cin>>n;
+
+    // int i=1;
+    // while(i<=n){
+        
+    //     char ch='A'+i-1;
+    //     int j=1;
+    //     while(j<=n){
+    //         cout<<ch;
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+
+    // 10
+    // ABCD
+    // ABCD
+    // ABCD
+    // ABCD
+    // int n;
+    // cout<<"Enter a number :";
+    // cin>>n;
+
+    // int i=1;
+    // while(i<=n){
+        
+    //     int j=1;
+    //     while(j<=n){
+    //         char ch='A'+j-1;
+    //         cout<<ch;
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+
+
+    // 11
+    // ABC
+    // DEF
+    // GHI
+
+    // int n;
+    // cout<<"Enter a number :";
+    // cin>>n;
+    // char ch='A';
+    // int i=1;
+    // while(i<=n){
+    //     int j=1;
+    //     while(j<=n){
+    //         cout<<ch;
+    //         ch++;
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+
+    
+    // 12
+    // ABC
+    // BCD
+    // CDE
+
+    // method 1
+    // int n;
+    // cout<<"Enter a number :";
+    // cin>>n;
+    // int i=1;
+    // while(i<=n){
+
+    //     int j=1;
+    //     char ch='A'+i-1;
+    //     while(j<=n){
+    //         cout<<ch;
+    //         ch++;
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+
+    // method 2
+    // int n;
+    // cout<<"Enter a number :";
+    // cin>>n;
+    // int i=1;
+    // while(i<=n){
+
+    //     int j=1;
+    //     while(j<=n){
+    //         char ch= j+i+'A'-2;
+    //         cout<<ch;
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+
+
+    // 13
+    // A
+    // BB
+    // CCC
+    // DDDD
+    // EEEEE
+
+    // int n;
+    // cout<<"Enter a number :";
+    // cin>>n;
+    // int i=1;
+    // while(i<=n){
+
+    //     int j=1;
+    //     while(j<=i){
+    //         char ch='A'+i-1;
+    //         cout<<ch;
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+    
+
+    //14
+    // A
+    // BC
+    // DEF
+    // GHIJ
+
+    // int n;
+    // cout<<"Enter a number :";
+    // cin>>n;
+
+    // char ch='A';
+    // int i=1;
+    // while(i<=n){
+
+    //     int j=1;
+    //     while(j<=i){
+    //         cout<<ch;
+    //         ch++;
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+
+
+    //15
+    
+    int n;
+    cout<<"Enter a number :";
+    cin>>n;
+
+    int i=1;
+    while(i<=n){
+           
+        int j=1;
+        while(j<=i){
+            char ch= j+i+'A'-2;
+            cout<<ch;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+
+
     return 0;
 }
